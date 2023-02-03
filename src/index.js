53da5a062d3c6591b3200899b01899fb158d72ba
Nikita <nikita@oden.com>
2023-02-04 01:51:16 +0300

    Use prettier again

--- src/index.js
+++ src/index.js
@@ -1,45 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { useState, useEffect, useRef } from 'react';
-import './style.css'
-import broom from './icon/clean.svg'
-import cancel from './icon/cancel.svg'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { useState, useEffect, useRef } from "react";
+import "./style.css";
+import broom from "./icon/clean.svg";
+import cancel from "./icon/cancel.svg";
 
-function SearchBar({}){
-  const [inputValue, setInputValue] = useState('');
-  const clearInput = () =>{
-    setInputValue('')
-}
+function SearchBar({}) {
+  const [inputValue, setInputValue] = useState("");
+  const clearInput = () => {
+    setInputValue("");
+  };
   const handleChange = (e) => {
-    setInputValue(e.target.value)
-}
+    setInputValue(e.target.value);
+  };
   return (
     <div className="searchBar-container">
       <div>
-        <input placeholder="Поиск по имени или e-mail" value={inputValue} onChange={handleChange}/>
+        <input
+          placeholder="Поиск по имени или e-mail"
+          value={inputValue}
+          onChange={handleChange}
+        />
       </div>
       <button onClick={clearInput}>
-        <img src={broom} alt=""/>
+        <img src={broom} alt="" />
         Очистить фильтр
       </button>
     </div>
-  )
+  );
 }
 
-function SortingOptions(){
+function SortingOptions() {
   return (
     <div className="sorting">
       <span>Сортировка:</span>
-      <input id="registration-date" name="sort" checked type="radio"/><label for="registration-date">Дата регистрации</label>
-      <input id="rating" name="sort" type="radio"/><label for="rating">Рейтинг</label>
+      <input id="registration-date" name="sort" checked type="radio" />
+      <label for="registration-date">Дата регистрации</label>
+      <input id="rating" name="sort" type="radio" />
+      <label for="rating">Рейтинг</label>
     </div>
-  )
+  );
 }
 
 function ModalWindow(props) {
   return (
-    <div className={props.modalActive ? 'overlay active' : 'overlay'}>
-      <div className='modal-window'>
+    <div className={props.modalActive ? "overlay active" : "overlay"}>
+      <div className="modal-window">
         <p>Вы уверены, что хотите удалить пользователя?</p>
         <div>
           <button onClick={() => props.handleDelete()}>Да</button>
@@ -47,30 +53,36 @@ function ModalWindow(props) {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-function Pagination({userPerPage, totalUsers, changePage}) {
-  const pageNumbers = []
+function Pagination({ userPerPage, totalUsers, changePage }) {
+  const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalUsers/userPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(totalUsers / userPerPage); i++) {
     pageNumbers.push(i);
   }
 
   return (
     <div>
       <ul className="pagination">
-        {
-          pageNumbers.map(number => {
-            return <li className="page-link" key={number} onClick={() => changePage(number)}>{number}</li>
-          })
-        }
+        {pageNumbers.map((number) => {
+          return (
+            <li
+              className="page-link"
+              key={number}
+              onClick={() => changePage(number)}
+            >
+              {number}
+            </li>
+          );
+        })}
       </ul>
     </div>
-  )
+  );
 }
 
-function UserDisplay(){
+function UserDisplay() {
   const [userArr, setUserArr] = useState([]);
   const [modalActive, setModalActive] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
@@ -80,24 +92,24 @@ function UserDisplay(){
 
   useEffect(() => {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://5ebbb8e5f2cfeb001697d05c.mockapi.io/users")
-    xhr.send()
-    xhr.responseType = 'json'
-    xhr.onload = function() {
+    xhr.open("GET", "https://5ebbb8e5f2cfeb001697d05c.mockapi.io/users");
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = function () {
       setUserArr(xhr.response);
-    }
-  }, [])
-  
+    };
+  }, []);
+
   const convertDate = (value) => {
     function addZero(num) {
-      return num.toString().padStart(2, '0');
+      return num.toString().padStart(2, "0");
     }
     const date = new Date(value);
     const month = addZero(date.getUTCMonth() + 1);
     const day = addZero(date.getDay() + 1);
     const year = addZero(date.getUTCFullYear());
     return year + "." + month + "." + day;
-  }
+  };
 
   function handleClick(id) {
     setModalActive(true);
@@ -105,13 +117,13 @@ function UserDisplay(){
   }
 
   function handleDelete() {
-    const arr = userArr.filter(e => e.key !== userIdRef.current)
+    const arr = userArr.filter((e) => e.key !== userIdRef.current);
     setUserArr(arr);
     setModalActive(false);
   }
 
   function changePage(pageNumber) {
-    setCurrentPage(pageNumber)
+    setCurrentPage(pageNumber);
   }
 
   const lastUserIndex = currentPage * userPerPage;
@@ -132,52 +144,58 @@ function UserDisplay(){
             </tr>
           </thead>
           <tbody>
-          .userArr.map(e => {
-                return (
-                  <tr key={e.id}>
-                    <td>{e.username}</td>
-                    <td>{e.email}</td>
-                    <td>{convertDate(e.registration_date)}</td>
-                    <td>{e.rating}</td>
-                    <td><img src={cancel} className="cancelBtn" alt="" width="18px" onClick={() => handleClick(e.id)}/></td>
-                  </tr>
-                )
-              })
+            {userArr.map((e) => {
+              return (
+                <tr key={e.id}>
+                  <td>{e.username}</td>
+                  <td>{e.email}</td>
+                  <td>{convertDate(e.registration_date)}</td>
+                  <td>{e.rating}</td>
+                  <td>
+                    <img
+                      src={cancel}
+                      className="cancelBtn"
+                      alt=""
+                      width="18px"
+                      onClick={() => handleClick(e.id)}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
-      <ModalWindow 
+      <ModalWindow
         handleDelete={handleDelete}
-        modalActive={modalActive} 
+        modalActive={modalActive}
         setModalActive={setModalActive}
       />
-      <Pagination 
-        userPerPage={userPerPage} 
-        totalUsers={userArr.length} 
+      <Pagination
+        userPerPage={userPerPage}
+        totalUsers={userArr.length}
         changePage={changePage}
       />
     </>
-  )
+  );
 }
 
-class UserTable extends React.Component{
-  constructor(props){
+class UserTable extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <>
-        <SearchBar/>
-        <SortingOptions/>
-        <UserDisplay/>
+        <SearchBar />
+        <SortingOptions />
+        <UserDisplay />
       </>
-    )
+    );
   }
 }
 
-
-
-const root = ReactDOM.createRoot( document.getElementById('root'));
-root.render(<UserTable/>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<UserTable />);
